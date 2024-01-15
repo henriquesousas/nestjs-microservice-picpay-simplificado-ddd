7a06b5ee3acb36c149d4d47b39835c9f4a5a3327
henriquesousas <paulohenriquess2014@gmail.com>
2024-01-15 17:43:27 -0300

    feat: create a createUserUseCase

---
+++ libs/core/src/user/usecases/create-user.usecase.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Result, isError } from '../../../common/types/types';
+import { UseCaseWithParam } from '../../../common/usecase/usecase';
+import { CreateUserDto } from '../dtos/create-user.dto';
+import {
+  USER_REPOSITORY,
+  UserRepository,
+} from '../repositories/user.repository';
+import { Document } from '../domain/document';
+import { User } from '../domain/user';
+
+@Injectable()
+export class CreateUserUseCase
+  implements UseCaseWithParam<CreateUserDto, void>
+{
+  constructor(
+    @Inject(USER_REPOSITORY)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async execute(dto: CreateUserDto): Promise<Result<void>> {
+    const documentOrError = Document.create(dto.document, dto.userType);
+    if (isError(documentOrError)) {
+      return documentOrError;
+    }
+    const document = documentOrError;
+    await this.userRepository.create(new User({ ...dto, document }));
+  }
+}
