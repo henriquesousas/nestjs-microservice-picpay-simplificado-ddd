7c1034312d15deac2e8070a5275845b6f183e517
henriquesousas <paulohenriquess2014@gmail.com>
2024-01-17 16:25:09 -0300

    refactor: user usecase

--- libs/core/src/user/usecases/create-user.usecase.ts
+++ libs/core/src/user/usecases/create-user.usecase.ts
@@ -3,27 +3,49 @@ import { Result, isError } from '../../../common/types/types';
 import { UseCaseWithParam } from '../../../common/usecase/usecase';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import {
-  USER_REPOSITORY,
+  USER_REPOSITORY_TOKEN,
   UserRepository,
 } from '../repositories/user.repository';
 import { Document } from '../entities/document';
-import { User } from '../entities/user';
+import { DocumentValidator } from '../document.validator';
+import { UserModel } from '../entities/user.model';
+import { UserAlreadyExistException } from '../exceptions/user-already-exist.exception';
+
+export const CREATE_USER_USECASE_TOKEN = 'CreateUserUseCase';
 
 @Injectable()
 export class CreateUserUseCase
-  implements UseCaseWithParam<CreateUserDto, void>
+  implements UseCaseWithParam<CreateUserDto, UserModel>
 {
   constructor(
-    @Inject(USER_REPOSITORY)
+    @Inject(USER_REPOSITORY_TOKEN)
     private readonly userRepository: UserRepository,
+    private readonly validators: DocumentValidator[],
   ) {}
 
-  async execute(dto: CreateUserDto): Promise<Result<void>> {
-    const documentOrError = Document.create(dto.document, dto.userType);
+  async execute(dto: CreateUserDto): Promise<Result<UserModel>> {
+    const user = await this.userRepository.findByDocumentOrEmail(
+      dto.document,
+      dto.email,
+    );
+    if (user) {
+      return new UserAlreadyExistException();
+    }
+    const documentOrError = Document.create(
+      dto.document,
+      dto.documentType,
+      this.validators,
+    );
     if (isError(documentOrError)) {
       return documentOrError;
     }
     const document = documentOrError;
-    await this.userRepository.create(new User({ ...dto, document }));
+    return await this.userRepository.create(
+      new UserModel({
+        ...dto,
+        document: document.getValue,
+        documentType: document.getType,
+      }),
+    );
   }
 }
