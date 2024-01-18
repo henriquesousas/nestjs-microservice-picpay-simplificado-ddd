a11059b24ab22e34c2259e86537f35541cb007fb
henriquesousas <paulohenriquess2014@gmail.com>
2024-01-18 15:45:36 -0300

    refactor:  document exception to any document

--- libs/core/src/feature/user/exceptions/document-invalid.exception.ts
+++ libs/core/src/feature/user/exceptions/document-invalid.exception.ts
@@ -1,12 +1,8 @@
 import { BadRequestException } from '@nestjs/common';
 import { DocumentType } from '../entities/document_type';
 
-//TODO: Refactor
 export class DocumentInvalidException extends BadRequestException {
   constructor(type: DocumentType) {
-    if (type === DocumentType.CNPJ) {
-      super('CNPJ inválido');
-    }
-    super('CPF inválido');
+    super(`${type} inválido`);
   }
 }
