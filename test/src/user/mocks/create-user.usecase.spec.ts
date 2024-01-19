83ed3ac7a8524259b4f587a61f046c31ddd561b4
henriquesousas <paulohenriquess2014@gmail.com>
2024-01-19 10:02:00 -0300

    test: userController

---
+++ test/src/user/mocks/create-user.usecase.spec.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Result } from '../../../../libs/core/src/common/types/types';
+import { CreateUserDto } from '../../../../libs/core/src/feature/user/dtos/create-user.dto';
+import { UserModel } from '../../../../libs/core/src/feature/user/entities/user.model';
+import { CreateUser } from '../../../../libs/core/src/feature/user/usecases/interfaces/create-user';
+import { userModelMock } from '../../../libs/core/user/mocks/user-model.mock';
+
+export class CreateUserUseCaseStub implements CreateUser {
+  async execute(dto: CreateUserDto): Promise<Result<UserModel>> {
+    return Promise.resolve(userModelMock);
+  }
+}
---
+++ test/src/user/user.controller.spec.ts
@@ -0,0 +1,51 @@
+import { CreateUserDto } from '../../../libs/core/src/feature/user/dtos/create-user.dto';
+import { UserController } from '../../../src/user/user.controller';
+import { DocumentType } from '../../../libs/core/src/feature/user/entities/document_type';
+import { BadRequestException } from '@nestjs/common';
+import { CreateUserUseCaseStub } from './mocks/create-user.usecase.spec';
+
+const makeDto = (
+  documentType: DocumentType = DocumentType.CPF,
+): CreateUserDto => {
+  return {
+    firstName: 'any_name',
+    secondName: 'any_second_name',
+    amount: 1,
+    document: 'any_document',
+    documentType,
+    email: 'any_email',
+    password: 'any_password',
+  };
+};
+
+describe('UserController', () => {
+  it('should create a new user', async () => {
+    const useCase = new CreateUserUseCaseStub();
+    const sut = new UserController(useCase);
+    const userModel = await sut.create(makeDto());
+    expect(userModel).toBeTruthy();
+    expect(userModel.firstName).toEqual(userModel.firstName);
+    expect(userModel.secondName).toEqual(userModel.secondName);
+    expect(userModel.email).toEqual(userModel.email);
+    expect(userModel.amount).toEqual(userModel.amount);
+  });
+
+  it('should retutn an error if usecase fails', async () => {
+    const useCase = new CreateUserUseCaseStub();
+    jest.spyOn(useCase, 'execute').mockImplementationOnce(() => {
+      return Promise.resolve(new BadRequestException('any_error'));
+    });
+    const sut = new UserController(useCase);
+    const data = sut.create(makeDto());
+    expect(data).rejects.toThrow(BadRequestException);
+  });
+
+  it('should call usecase with correct values', async () => {
+    const useCase = new CreateUserUseCaseStub();
+    const usecaseSpy = jest.spyOn(useCase, 'execute');
+    const sut = new UserController(useCase);
+    const dto = makeDto();
+    await sut.create(dto);
+    expect(usecaseSpy).toHaveBeenCalledWith(dto);
+  });
+});
