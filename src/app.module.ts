60ef0ddd93674d1c78a797f9a45e2273b019b980
henriquesousas <paulohenriquess2014@gmail.com>
2024-01-21 19:52:47 -0300

    refacto: imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModel } from '../libs/core/src/feature/user/entities/user.model';
+import { UserModel } from '../libs/core/src/feature/user/models/user.model';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from '../libs/core/src/common/database/database.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { TransactionModel } from '../libs/core/src/feature/transaction/models/transaction.model';
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
-    DatabaseModule.forFeature([UserModel]),
+    DatabaseModule.forFeature([UserModel, TransactionModel]),
     UserModule,
+    TransactionModule,
   ],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, UserModule],
 })
 export class AppModule {}
