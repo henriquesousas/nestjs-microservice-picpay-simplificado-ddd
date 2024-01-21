53477d1588692e5146ea1105a6771fcb4120673f
henriquesousas <paulohenriquess2014@gmail.com>
2024-01-21 19:43:41 -0300

    refactor: change abstracy entity

--- libs/core/src/common/database/abstract-entity.ts
+++ libs/core/src/common/database/abstract-entity.ts
@@ -1,8 +1,18 @@
-import { PrimaryGeneratedColumn } from 'typeorm';
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export class AbstractEntity<T> {
   @PrimaryGeneratedColumn()
   id: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
 
   constructor(entity: Partial<T>) {
     Object.assign(this, entity);
