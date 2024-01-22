65c35f850a2f6025080b57808fe3b0ec8c55552b
henriquesousas <paulohenriquess2014@gmail.com>
2024-01-22 15:25:24 -0300

    feat: get number port from env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from '../libs/core/src/common/filter/http-exception.filter';
+import { HttpExceptionFilter } from '@app/core/common/filter/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +15,6 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
-  await app.listen(3000);
+  await app.listen(configService.get('HTTP_PORT'));
 }
 bootstrap();
