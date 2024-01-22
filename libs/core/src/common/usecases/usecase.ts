2a4fd4a0acbab96e0a266865fb8ddc56559059b8
henriquesousas <paulohenriquess2014@gmail.com>
2024-01-22 15:14:27 -0300

    refactor: remove from project

--- libs/core/src/common/usecases/usecase.ts
+++
@@ -1,9 +0,0 @@
-import { Result } from '../types/types';
-
-export interface UseCaseWithParam<In, Out> {
-  execute(data: In): Promise<Result<Out>>;
-}
-
-export interface UseCaseWithoutParam<Out> {
-  execute(): Promise<Result<Out>>;
-}
