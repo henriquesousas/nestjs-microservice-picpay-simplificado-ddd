f35cbf2a6b27ced2eac3871b4592e74d414ffaf2
henriquesousas <paulohenriquess2014@gmail.com>
2024-05-11 18:39:36 -0300

    feat: add a domain usecase

---
+++ src/customer/domain/usecase/CreateCustomerUseCase.ts
@@ -0,0 +1,54 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Result } from '../../../../libs/core/src/common/types/types';
+
+import { CustomerBuild } from '../build/CustomerBuild';
+import { CreateCustomerDto } from '../dto/CreateCustomerDto';
+import { CustomerCreatedEvent } from '../events/CustomerCreatedEvent';
+import {
+  CUSTOMER_REPOSITORY_TOKEN,
+  CustomerRepository,
+} from '../repository/CustomerRepository';
+import {
+  Dispatcher,
+  EVENT_DISPATCHER_TOKERN,
+} from '../../../@shared/event/Dispatcher';
+import { CustomerAlreadyExistException } from '../exception/CustomerAlreadyExistException';
+
+export const CREATE_CUSTOMER_USECASE = 'CreateCustomer';
+
+export interface CreateCustomer {
+  execute(dto: CreateCustomerDto): Promise<Result<void>>;
+}
+
+@Injectable()
+export class CreateCustomerUseCase implements CreateCustomer {
+  constructor(
+    @Inject(CUSTOMER_REPOSITORY_TOKEN)
+    private readonly customerRepository: CustomerRepository,
+    @Inject(EVENT_DISPATCHER_TOKERN)
+    private readonly eventDispatcher: Dispatcher,
+    private readonly eventName: string,
+  ) {}
+
+  async execute(dto: CreateCustomerDto): Promise<Result<void>> {
+    const customerFromDb = await this.customerRepository.getByEmailOrDocument(
+      dto.email,
+      dto.document,
+    );
+    if (customerFromDb) {
+      return new CustomerAlreadyExistException();
+    }
+    const customer = new CustomerBuild(
+      dto.firstName,
+      dto.surName,
+      dto.email,
+      dto.password,
+      dto.document,
+      dto.documentType,
+    )
+      .withWallet(dto.amount)
+      .build();
+    await this.customerRepository.save(customer);
+    this.eventDispatcher.notify(new CustomerCreatedEvent(this.eventName, dto));
+  }
+}
