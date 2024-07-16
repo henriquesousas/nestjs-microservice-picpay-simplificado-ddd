6420a0199ebea9f73a8e1e94fe34a1711244ef48
henriquesousas <paulohenriquess2014@gmail.com>
2024-07-16 22:11:52 +0000

    feat: adicionado presenters

--- src/nest-module/customer/customer.controller.ts
+++ src/nest-module/customer/customer.controller.ts
@@ -5,12 +5,13 @@ import {
   HttpCode,
   Inject,
   Param,
+  ParseUUIDPipe,
   Post,
 } from '@nestjs/common';
 import { CreateCustomerRequestDto } from './dtos/create-customer-request.dto';
 import { CreateCustomerUseCase } from '../../core/customer/application/usecase/create/create-customer.usecase';
 import { GetCustomerByIdUseCase } from '../../core/customer/application/usecase/get-customer/get-customer-by-id.usecase';
-import { CustomerPresenter } from './customer.presenter';
+import { CustomerPresenter, WalletPresenter } from './customer.presenter';
 import { GetBalanceUseCase } from '../../core/customer/application/usecase/get-balance/get-balance.usecase';
 
 @Controller('customer')
@@ -19,7 +20,7 @@ export class CustomerController {
   private readonly createCustomerUseCase: CreateCustomerUseCase;
 
   @Inject(GetCustomerByIdUseCase)
-  private readonly getCustomerByEmailUseCase: GetCustomerByIdUseCase;
+  private readonly getCustomerUseCase: GetCustomerByIdUseCase;
 
   @Inject(GetBalanceUseCase)
   private readonly getBalanceUseCase: GetBalanceUseCase;
@@ -45,10 +46,11 @@ export class CustomerController {
   @Get('/:customerId')
   @HttpCode(200)
   async getCustomer(
-    @Param('customerId') customerId: string,
+    @Param('customerId', new ParseUUIDPipe({ errorHttpStatusCode: 422 }))
+    customerId: string,
   ): Promise<CustomerPresenter> {
     const [customer, error] = (
-      await this.getCustomerByEmailUseCase.execute(customerId)
+      await this.getCustomerUseCase.execute(customerId)
     ).asArray();
 
     if (error) {
@@ -60,14 +62,15 @@ export class CustomerController {
   @Get('/balance/:customerId')
   @HttpCode(200)
   async getBalance(
-    @Param('customerId') customerId: string,
-  ): Promise<{ balance: number }> {
+    @Param('customerId', new ParseUUIDPipe({ errorHttpStatusCode: 422 }))
+    customerId: string,
+  ): Promise<WalletPresenter> {
     const [wallet, error] = (
       await this.getBalanceUseCase.execute(customerId)
     ).asArray();
     if (error) {
       throw error;
     }
-    return { balance: wallet.balance };
+    return WalletPresenter.build(wallet);
   }
 }
--- src/nest-module/customer/customer.presenter.ts
+++ src/nest-module/customer/customer.presenter.ts
@@ -1,19 +1,34 @@
 import { Transform } from 'class-transformer';
-import { Customer } from '../../core/customer/domain/entity/customer';
+import {
+  Customer,
+  DocumentType,
+} from '../../core/customer/domain/entity/customer';
+import { Wallet } from '../../core/customer/domain/entity/wallet';
+import { Document } from '../../../libs/common/src/core/entity/document';
+
+export class WalletPresenter {
+  private constructor(private wallet_id: string, private balance: number) {}
+
+  static build(wallet: Wallet): WalletPresenter {
+    return new WalletPresenter(wallet.getUUid().id, wallet.balance);
+  }
+}
+
+export class DocumentPresenter {
+  private constructor(private number: string, private type: DocumentType) {}
+
+  static build(document: Document): DocumentPresenter {
+    return new DocumentPresenter(document.getValue(), document.getType());
+  }
+}
 
 export class CustomerPresenter {
   private customer_id: string;
   private first_name: string;
   private sur_name: string;
   private email: string;
-  private document: {
-    number: string;
-    type: string;
-  };
-  private wallet: {
-    id: string;
-    balance: number;
-  };
+  private document: DocumentPresenter;
+  private wallet: WalletPresenter;
   private canTransfer: boolean;
   @Transform(({ value }: { value: Date }) => value.toISOString())
   private created_at: Date;
@@ -23,14 +38,8 @@ export class CustomerPresenter {
     this.first_name = customer.props.name.getfirstName;
     this.sur_name = customer.props.name.getSurName;
     this.email = customer.props.email.getEmail();
-    this.document = {
-      number: customer.props.document.getValue(),
-      type: customer.props.document.getType(),
-    };
-    this.wallet = {
-      id: customer.props.wallet?.getUUid().id!,
-      balance: customer.props.wallet?.balance!,
-    };
+    this.document = DocumentPresenter.build(customer.props.document);
+    this.wallet = WalletPresenter.build(customer.props.wallet!);
     this.canTransfer = customer.canMakeTransfer();
     this.created_at = this.created_at = customer.props.createdAt!;
   }
